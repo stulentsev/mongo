3e35f013e686a1edb3b44d5a0c1480a48cabb74c
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2011-03-20 07:51:29 +0300

    fix quotes in text

--- jstests/distinct_count1.js
+++ jstests/distinct_count1.js
@@ -12,14 +12,14 @@ t.save( { a : 4 } )
 t.save( { a : 4 } )
 
 // normal form
-res = db.runCommand( { distinct : 'distinctCount1' , key : 'a' , { } , count : true } );
-assert.eq( 4 , res[ 'count' ] , "A1" );
+res = db.runCommand( { distinct : "distinctCount1" , key : "a" , { } , count : true } );
+assert.eq( 4 , res[ "count" ] , "A1" );
 
 // value of count != true, ignore it and fall back to default behaviour
-res = db.runCommand( { distinct : 'distinctCount1' , key : "a" , { a : { $lt : 3 } }, count : 2 } );
-assert.eq( "1,2" , res[ 'values' ] , "A2" );
-assert.eq( undefined , res[ 'count' ] , 'A3' );
+res = db.runCommand( { distinct : "distinctCount1" , key : "a" , { a : { $lt : 3 } }, count : 2 } );
+assert.eq( "1,2" , res[ "values" ] , "A2" );
+assert.eq( undefined , res[ "count" ] , "A3" );
 
-res = db.runCommand( { distinct : 'distinctCount1' , key : "a" , { a : { $lt : 3 } }, count : "true" } );
-assert.eq( "1,2" , res[ 'values' ] , "A2" );
-assert.eq( undefined , res[ 'count' ] , 'A3' );
+res = db.runCommand( { distinct : "distinctCount1" , key : "a" , { a : { $lt : 3 } }, count : "true" } );
+assert.eq( "1,2" , res[ "values" ] , "A2" );
+assert.eq( undefined , res[ "count" ] , "A3" );
