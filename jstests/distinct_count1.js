f6b20a83538ce40a626b54dcdc8ca4488b194a06
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2011-03-20 07:48:49 +0300

    fix test again

--- jstests/distinct_count1.js
+++ jstests/distinct_count1.js
@@ -13,13 +13,13 @@ t.save( { a : 4 } )
 
 // normal form
 res = db.runCommand( { distinct : 'distinctCount1' , key : 'a' , { } , count : true } );
-assert.eq( 4 , res.count , "A1" );
+assert.eq( 4 , res[ 'count' ] , "A1" );
 
 // value of count != true, ignore it and fall back to default behaviour
 res = db.runCommand( { distinct : 'distinctCount1' , key : "a" , { a : { $lt : 3 } }, count : 2 } );
-assert.eq( "1,2" , res.values , "A2" );
-assert.eq( undefined , res.count , 'A3' );
+assert.eq( "1,2" , res[ 'values' ] , "A2" );
+assert.eq( undefined , res[ 'count' ] , 'A3' );
 
 res = db.runCommand( { distinct : 'distinctCount1' , key : "a" , { a : { $lt : 3 } }, count : "true" } );
-assert.eq( "1,2" , res.values , "A4" );
-assert.eq( undefined , res.count , 'A5' );
+assert.eq( "1,2" , res[ 'values' ] , "A2" );
+assert.eq( undefined , res[ 'count' ] , 'A3' );
